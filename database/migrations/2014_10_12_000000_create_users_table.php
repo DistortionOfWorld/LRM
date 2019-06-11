1b801a0ac2f52829918197fd9658ce4e505bd01e
DistortionOfWorld <vm.yaruo@gmail.com>
2019-06-11 16:07:26 +0900

    update migrate file

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -20,9 +20,9 @@ class CreateUsersTable extends Migration
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
             $table->rememberToken();
-            $table->string('language')->default("ja");
-            $table->string('theme_primary')->default("indigo");
-            $table->string('theme_secondary')->default("pink");
+            $table->string('language')->default('ja');
+            $table->string('theme_primary')->default('indigo');
+            $table->string('theme_secondary')->default('pink');
             $table->timestamps();
         });
     }
